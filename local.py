90741acd14242478127a12215bee53e7843eb8b3
Frittutisna <frittutisna@gmail.com>
2026-06-23 01:08:18 +0900

    Removed test

--- local.py
+++ local.py
@@ -54,4 +54,4 @@ if __name__ == "__main__":
                 root.destroy()
                 sys.exit(1)
                 
-        for analyzer in analyzer_pool: analyzer.process_and_generate() # Test
+        for analyzer in analyzer_pool: analyzer.process_and_generate()
